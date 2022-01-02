938dfb157cf8308ca7cda86885765467f10569de
unknown <codewithsrobins1@gmail.com>
2022-01-02 09:51:50 -0800

    built out displaying posts and using graphql to grab info

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,8 @@
 import Head from 'next/head';
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const posts = [
-  {
-    title: 'React Testing',
-    excerpt: 'Learn react testing'
-  },
-  {
-    title: 'React with tailwind',
-    excerpt: 'Learn react with tailwind'
-  }
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -22,7 +12,7 @@ export default function Home() {
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => <PostCard post={post} key={post.title} /> )}
+          {posts.map((post, index) => <PostCard post={post.node} key={post.title} /> )}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
@@ -33,4 +23,15 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+//Fetch Data Inside Component using getStaticProps
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
